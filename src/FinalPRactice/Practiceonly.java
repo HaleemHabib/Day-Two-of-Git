c40cd90aea42a4dafb5bce7b3d4c6591158287f4
Haleem Habib <haleem.habib07@gmail.com>
2022-06-21 20:29:37 -0400

    Alert New Codes Added Here

---
+++ src/FinalPRactice/Practiceonly.java
@@ -0,0 +1,15 @@
+package FinalPRactice;
+
+public class Practiceonly {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		String name = "Fat Boy";
+		String LastName = "But Smart";
+		System.out.println("Good Practice==============Keep it up");
+	}
+
+}
---
+++ src/FinalPRactice/PushCodesHere.java
@@ -0,0 +1,14 @@
+package FinalPRactice;
+
+public class PushCodesHere {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int a = 10;
+	int b = 50;
+	int z = a+b;
+	System.out.println("-----------main----------------");
+	}
+
+}
