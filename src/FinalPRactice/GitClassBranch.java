179e4b71dc9b790a2cfd16470f991c0e2c1dae10
Haleem Habib <haleem.habib07@gmail.com>
2022-06-22 12:25:24 -0400

    Class added after push

---
+++ src/FinalPRactice/GitClassBranch.java
@@ -0,0 +1,11 @@
+package FinalPRactice;
+
+public class GitClassBranch {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Learning git was easy");
+	}
+
+}
